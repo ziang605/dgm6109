1e39508b8039fd3087e46a3b6315bf3dcbf331ce
ziang605 <ziang605@gmail.com>
2026-01-14 19:48:00 -0800

    I finished the lab1 assignment
    I finished the lab1 assignment

---
+++ lab1/temperatureConverter.js
@@ -0,0 +1,7 @@
+let fahrenheit = 80 // Beijing Augest 2023
+let celsiue = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5/ 9
+
+console.log("temperature(clesiue) " + celsiue)
+console.log("temperature(kelvin) " + kelvin)
+console.log("temperature(fahrenheit)" + fahrenheit)
